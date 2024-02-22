f7fa8ba35429efe418ec720be26ed82b848d0452
Aimé <aimelimhountchen@gmail.com>
2024-02-22 11:58:51 +0100

    feat: added Atelier.ts

---
+++ backend/src/data-management/Atelier.ts
@@ -0,0 +1,11 @@
+class Atelier {
+    private id: number;
+    private theme: Theme;
+    private nbPlaces: number;
+
+    constructor(id: number, theme: Theme, nbPlaces: number) {
+        this.id = id;
+        this.theme = theme;
+        this.nbPlaces = nbPlaces;
+    }
+}
