2da712047c7e0b0b37bd4df04dda243164b1a388
zhisheng <zhishengtrieu@gmail.com>
2024-02-22 13:29:36 +0100

    feat(backend): add new routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,8 +5,85 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //Pour l'user
+  @Get('/candidater')
+  candidaterAtelier() {
+    return this.appService.candidater();
   }
+
+  //Pour l'admin
+  @Get('/ateliers')
+  getAteliers() {
+    return this.appService.getAteliers();
+  }
+
+  @Get('/candidats')
+  listeCandidats() {
+    return this.appService.listeCandidats();
+  }
+
+  /*
+  @Get('/labels')
+  listeLabels()  {
+    return this.appService.listeLabels();
+  }
+
+  @Get('/ajouterLabel')
+  ajouterLabel() {
+    return this.appService.ajouterLabel();
+  }
+
+  @Get('/modifierLabel')
+  modifierLabel() {
+    return this.appService.modifierLabel();
+  }
+
+  @Get('/supprimerLabel')
+  supprimerLabel() {
+    return this.appService.supprimerLabel();
+  }*/
+
+  @Get('/ajouterAtelier')
+  ajouterAtelier() {
+    return this.appService.ajouterAtelier();
+  }
+
+  @Get('/modifierAtelier')
+  modifierAtelier() {
+    return this.appService.modifierAtelier();
+  }
+
+  @Get('/supprimerAtelier')
+  supprimerAtelier() {
+    return this.appService.supprimerAtelier();
+  }
+
+  /**
+   * Permet de repartir les candidats en attente dans les ateliers 
+   * @returns 
+   */
+  @Get('/repartirCandidats')
+  repartirCandidats() {
+    return this.appService.repartirCandidats();
+  }
+
+  /**
+   * Permet d'anuler la repartition d'un candidat dans un atelier
+   * le ramenne dans la liste des candidats en attente
+   * @returns 
+   */
+  @Get('/anulerRepartition')
+  anulerRepartition() {
+    return this.appService.anulerRepartition();
+  }
+
+  /**
+   * Supprime un candidat de la liste des candidats en attente
+   * @returns 
+   */
+  @Get('/supprimerCandidat')
+  supprimerCandidat() {
+    return this.appService.supprimerCandidat();
+  }
+
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,63 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  //Pour l'user
+  candidaterAtelier() {
+    throw new Error('Method not implemented.');
+  }
+
+  //Pour l'admin
+  getAteliers() {
+    throw new Error('Method not implemented.');
+  }
+
+  listeCandidats() {
+    throw new Error('Method not implemented.');
+  }
+  
+  candidater() {
+    throw new Error('Method not implemented.');
+  }
+
+  ajouterAtelier() {
+    throw new Error('Method not implemented.');
+  }
+
+  modifierAtelier() {
+    throw new Error('Method not implemented.');
+  }
+
+  supprimerAtelier() {
+    throw new Error('Method not implemented.');
+  }
+
+  listeLabels() {
+    throw new Error('Method not implemented.');
+  }
+
+  /*
+  ajouterLabel() {
+    throw new Error('Method not implemented.');
+  }
+
+  modifierLabel() {
+    throw new Error('Method not implemented.');
+  }
+
+  supprimerLabel() {
+    throw new Error('Method not implemented.');
+  }*/
+
+  repartirCandidats() {
+    throw new Error('Method not implemented.');
+  }
+
+  anulerRepartition(){
+    throw new Error('Method not implemented.');
+  }
+
+  supprimerCandidat() {
+    throw new Error('Method not implemented.');
   }
 }
