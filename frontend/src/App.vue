e085fb3485032f1b9a17fd008cd54357b50ba91d
Paul <paul5mass5@gmail.com>
2024-02-22 12:22:59 +0100

    initial frontend page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,56 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <img alt="Vue logo" src="./assets/logo_boite_a_cuisine.png" id="logo">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView/>
+  </main>
+
+  <footer>
+
+  </footer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
+  position: relative;
+  background-color: white;
   line-height: 1.5;
-  max-height: 100vh;
-}
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  img {
+    position: absolute;
+    left: 0;
+    height: inherit;
+    margin-right: 2em;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    a {
+      margin: 0 1em;
+    }
   }
+}
+
+main {
+  padding: 20px;
+}
+
+footer {
+  line-height: 1.5;
+  height: 80px;
+  background-color: white;
+  box-shadow: 0 -2px 5px 0 rgba(0, 0, 0, 0.26);
 }
 </style>
